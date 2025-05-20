654562532276d4b2807b815e3048ffdcf2b595e6
ryoga911 <zeal24360044@fsgcl.jp>
2025-05-20 12:16:17 +0900

    no message

--- src/start.java
+++ src/start.java
@@ -1,8 +1,10 @@
-package javaForBeginners;
+
 
 public class start {
 
 	public static void main(String[] args) {
+		System.out.println("でぃびじょん");
+		MyFrame frame1 = new RectTest();
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
