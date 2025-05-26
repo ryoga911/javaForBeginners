5ac0ac2ff1ec01d98b374b72ff97e0b67be5e3e1
ryoga911 <zeal24360044@fsgcl.jp>
2025-05-26 11:37:15 +0900

    no message

---
+++ src/RectAndOver.java
@@ -0,0 +1,11 @@
+
+public class RectAndOver extends MyFrame {
+	public void run() {
+	fillRect(40,50,40,20);
+	fillRect(20,70,50,40);
+	fillRect(30,90,20,20);
+	fillRect(70,90,40,40);
+	
+	
+
+}}
--- src/start.java
+++ src/start.java
@@ -4,7 +4,7 @@ public class start {
 
 	public static void main(String[] args) {
 		System.out.println("でぃびじょん");
-		MyFrame frame1 = new TextTest();
+		MyFrame frame1 = new RectAndOver();
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
