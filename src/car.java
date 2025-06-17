087d40be6473fde56dfcd297fd2306b7d44df1a7
ryoga911 <zeal24360044@fsgcl.jp>
2025-06-17 10:58:58 +0900

    上下運動

--- src/car.java
+++ src/car.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class car extends Vehicle {
+public class car extends Vehicle implements KeyListener {
 	public car(int x, int y, int vx, int vy) {
 		super(x, y, vx, vy);
 	}
@@ -9,6 +11,50 @@ public class car extends Vehicle {
 		frame.fillRect(x, y+20, 80, 20);
 		frame.fillOval(x+10, y+40, 20, 20);
 		frame.fillOval(x+50, y+40, 20, 20);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+		vx=-5;
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+		if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=5;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_UP) {
+			vy=-10;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_DOWN) {
+			vy=10;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+		vx=0;
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+		if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=0;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_UP) {
+			vy=0;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_DOWN) {
+			vy=0;
+		}
+		// TODO 自動生成されたメソッド・スタブ
+		
 	}
 
 	}
--- src/start.java
+++ src/start.java
@@ -4,7 +4,7 @@ public class start {
 
 	public static void main(String[] args) {
 		
-		MyFrame f=new Displaycarstrains();
+		MyFrame f=new playeranimation();
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
